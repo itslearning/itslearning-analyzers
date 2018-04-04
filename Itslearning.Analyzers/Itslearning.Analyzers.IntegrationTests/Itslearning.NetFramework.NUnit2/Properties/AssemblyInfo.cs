dea3855e3afeb376de131905d9363fc635ae257d
Marcin Wachulski <marcin.wachulski@pragmasoft.pl>
2018-04-04 23:38:56 +0200

    Add integration tests.

---
+++ Itslearning.Analyzers/Itslearning.Analyzers.IntegrationTests/Itslearning.NetFramework.NUnit2/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Itslearning.NetFramework.NUnit2")]
+[assembly: AssemblyDescription("Integration tests project for Itslearning.Analyzers Roslyn project.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("itslearning AS")]
+[assembly: AssemblyProduct("Itslearning.Analyzers")]
+[assembly: AssemblyCopyright("Copyright (C) 2018")]
+[assembly: AssemblyTrademark("itslearning AS")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1ab9d211-a4f8-4739-b41b-7b97808981d7")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Itslearning.Analyzers/Itslearning.Analyzers.IntegrationTests/Itslearning.NetFramework.NUnit2/TestNamingTestsInfringingRules.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Itslearning.NetFramework.NUnit2
+{
+    [TestFixture]
+    public class TestNamingTestsInfringingRules
+    {
+        [Test]
+        public void BrokenNameTestMethod()
+        {
+        }
+    }
+}
